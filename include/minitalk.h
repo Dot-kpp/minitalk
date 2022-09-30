4c58b1034666881d3f3ab78dacffad575b0f74bf
bigrig <meowleneon.falcon@kppmail.com>
2022-09-29 22:15:44 -0400

    basic setup

---
+++ include/minitalk.h
@@ -0,0 +1,19 @@
+#ifndef MINITALK_H
+# define MINITALK_H
+
+#include <signal.h>
+#include <zconf.h>
+#include "libft.h"
+
+# define BUFFSIZE 1
+
+# ifndef TRUE
+#  define TRUE 1
+# endif
+
+# ifndef FALSE
+#  define FALSE 0
+# endif
+
+
+#endif
